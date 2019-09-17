7f7fd3a796c47ae4243c63fcd1a40c16e7eafaba
Trilok Khairnar <214651+cruizen@users.noreply.github.com>
2019-09-17 14:53:18 +0530

    Update TPCH_installer.sh
    Update URL of installer.sh to point to the forked repo

--- azure/TPCH_installer.sh
+++ azure/TPCH_installer.sh
@@ -3,7 +3,7 @@
 wget -O /tmp/HDInsightUtilities-v01.sh -q https://hdiconfigactions.blob.core.windows.net/linuxconfigactionmodulev01/HDInsightUtilities-v01.sh && source /tmp/HDInsightUtilities-v01.sh && rm -f /tmp/HDInsightUtilities-v01.sh
 
 if [[ `hostname -f` == `get_primary_headnode` ]]; then
-	wget https://github.com/dharmeshkakadia/tpch-datagen-as-hive-query/archive/master.zip
+	wget https://github.com/cruizen/tpch-hdinsight/archive/master.zip
 	unzip master.zip; cd tpch-datagen-as-hive-query-master;
 	hive -i settings.hql -f TPCHDataGen.hql -hiveconf SCALE=$1 -hiveconf PARTS=$1 -hiveconf LOCATION=/HiveTPCH_$1/ -hiveconf TPCHBIN=resources
 	hive -i settings.hql -f ddl/createAllExternalTables.hql -hiveconf LOCATION=/HiveTPCH_$1/ -hiveconf DBNAME=tpch_$1
